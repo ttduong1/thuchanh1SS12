a4e866f9ec64cecfff730796b4584d18ac0c2f74
duongt <trinhtungduong2019@gmail.com>
2022-11-15 16:09:20 +0700

    update

---
+++ demo1.js
@@ -0,0 +1,4 @@
+// let i;
+// for (i = 0; i < 5; i++) {
+//     text += "The number is" + 1 + "<br>";
+// }
---
+++ demo2.js
@@ -0,0 +1,7 @@
+let num = prompt("Enter your number: ");
+let total = 0;
+let i = 1;
+for (; i <= num; i += 1) {
+}
+total = total + i;
+alert(total);
